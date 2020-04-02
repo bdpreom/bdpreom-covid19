6968bd381a42a705c5142860c1b70411d0e08125
shadman sakib khan <preom.ssk@gmail.com>
2020-04-02 18:46:35 +0600

    Initial Commit

---
+++ CovidXray/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CovidXray
+//
+//  Created by shadman sakib khan on 4/2/20.
+//  Copyright © 2020 shadman sakib khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
